b447ba6818dd685ab618c612897b7e2f81a4a13b
lledune <r>
2019-04-18 22:29:43 +0200

    glm

--- R/glm.R
+++ R/glm.R
@@ -11,8 +11,35 @@ colnames(data)[length(data)] = "NumberClaims"
 
 library(caret)
 library(caTools)
-
+#install.packages("mlbench")
+library(mlbench)
+library(glmnet)
 #using cross validation with K=10
+
+#feature selection, using RFE https://machinelearningmastery.com/feature-selection-with-the-caret-r-package/
+
+control <- rfeControl(functions=rfFuncs, method="cv", number=10)
+
+fit = glmnet(as.matrix(data[,1:26]), as.matrix(data[,27]), family = "poisson")
+plot(fit, xvar = "dev", label = TRUE)
+
+cvfit = cv.glmnet(as.matrix(data[,1:26]), as.matrix(data[,27]), family = "poisson")
+plot(cvfit, xvar = "dev", label = TRUE)
+
+#One line is simply the ?? lambda corresponding to the minimum MSE of the cross validation (your left dotted line) . 
+#When adding one standard error to the minimum MSE value, you get a more regularized model, i.e. 
+#one that performs favorable for predicting purposes. The ?? value belonging to it is denoted by the right dotted line. 
+
+#accessing the values : 
+cvfit$lambda.min; cvfit$lambda.1se #Ususally better to choose lambda1se because it reduces overfitting
+
+coefLambda = coef(cvfit,s=cvfit$lambda.1se) #coefficient for lambda 1se
+
+#Only 10 coefficients are kept from feature selection 
+
+?barplot
+barplot(coefLambda@x)
+
 
 fitControl <- trainControl(## 10-fold CV
   method = "cv",
